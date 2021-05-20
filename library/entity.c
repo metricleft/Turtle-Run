ae278f262fd00968fc6c13a5a188dcf1c403eaaf
jwu7 <jwu7@caltech.edu>
2021-05-20 01:52:18 -0400

    Created "entity.h", allowing for more tags per body

---
+++ library/entity.c
@@ -0,0 +1,48 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include "entity.h"
+
+/*
+TODO:
+- entity stores a behavior function for enemies
+- entity stores some sort of code that corresponds to its sprite image
+*/
+
+/*
+List of entity_type tags:
+PLAYER - the player character. Stored at index 0 in the scene.
+ENEMY - a movable enemy with its own unique behavior.
+TERRAIN - a piece of terrain. Not fallable.
+POWERUP - a powerup with its own unique behavior. Includes coins.
+*/
+
+typedef struct entity {
+    char *entity_type;
+    bool scrollable;
+    bool fallable;
+} entity_t;
+
+entity_t *entity_init(char *entity_type, bool scrollable, bool fallable) {
+    entity_t *entity = malloc(sizeof(entity_t));
+    entity->entity_type = entity_type;
+    entity->scrollable = scrollable;
+    entity->fallable = fallable;
+    return entity;
+}
+
+void entity_free(entity_t *entity) {
+    free(entity->entity_type);
+    free(entity);
+}
+
+char *entity_get_type(entity_t *entity) {
+    return entity->entity_type;
+}
+
+bool entity_get_scrollable(entity_t *entity) {
+    return entity->scrollable;
+}
+
+bool entity_get_fallable(entity_t *entity) {
+    return entity->fallable;
+}
